76cc355c489b01817a5c51c7a80d4e47a7c61b8a
Hung Nguyen <nvhung10112001@gmail.com>
2024-08-06 22:08:40 +0700

    ví dụ 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const App(),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Nguyễn Viết Hưng"),
+            Text("10/11/2001"),
+            Text("nvhung10112001@gmail.com"),
+            ElevatedButton(
+              onPressed: () {
+                print("button pressed");
+              },
+              child: Text('hoang 1213'),
+            ),
+            Card(
+              color: Colors.red,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: 56,
+                  bottom: 22,
+                  left: 12,
+                  right: 42,
+                ),
+                child: Text("Hoang le"),
+              ),
+            ),
+          ],
         ),
       ),
     );
